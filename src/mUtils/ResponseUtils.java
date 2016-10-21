65e06ce3d80d7b2755659747a81f53230d0f0d9e
张武 <wuzhang4@creditease.cn>
2016-10-21 22:04:14 +0800

    统一配置客户端和服务端 Gson 解析 date 的格式

--- src/mUtils/ResponseUtils.java
+++ src/mUtils/ResponseUtils.java
@@ -6,12 +6,13 @@ import java.io.PrintWriter;
 import javax.servlet.http.HttpServletResponse;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 public class ResponseUtils {
 
 	public static void resposeToApp(HttpServletResponse response, Object data)
 			throws IOException {
-		Gson gson=new Gson();
+		Gson gson=new GsonBuilder().setDateFormat("yyyy-MM-dd'T'HH:mm:ss").create();
 		String resultJson=gson.toJson(data);
 		System.out.println(resultJson);
 		response.setContentType("text/json; charset=utf-8");
